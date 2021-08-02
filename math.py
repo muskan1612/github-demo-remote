b9d41a2da713126820525d1c11b6944758799c8e
muskan1612 <61726545+muskan1612@users.noreply.github.com>
2021-08-02 14:32:52 +0530

    math.py initial commit

---
+++ math.py
@@ -0,0 +1,15 @@
+#function to add two numbers
+def add(x,y):
+	return x+y
+
+#function to subtract two numbers
+def sub():
+	pass
+
+#function to multiply two numbers
+def mul(x,y):
+	return x*y
+
+#function to divide two numbers
+def div():
+	pass
